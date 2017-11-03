bf76ec8b72a593156048cb66c7a7bee51c85e605
sjacobs146 <susanjacobs@verizon.net>
2017-11-03 16:15:28 -0400

    Add Update button to job template
    - Add link-to from job template to jobs-edit so user can edit job
    - Add transitionTo in jobs-edit so when update is complete, user
    is returned to job template.

--- app/routes/jobs-edit.js
+++ app/routes/jobs-edit.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
   model (params) {
     return this.get('store').findRecord('job', params.job_id)
   },
@@ -12,6 +13,7 @@ export default Ember.Route.extend({
           data.set('title', job.title)
           data.set('url', job.url)
           data.set('status', job.status)
+          data.set('dateApplied', job.dateApplied)
           data.set('recruiterName', job.recruiterName)
           data.set('recruiterEmail', job.recruiterEmail)
           data.set('recruiterPhone', job.recruiterPhone)
@@ -22,6 +24,7 @@ export default Ember.Route.extend({
           this.get('flashMessages')
           .success('Successfully updated your job!');
         })
+        .then(() => this.transitionTo('job', job))
         .catch(() => {
           this.get('flashMessages')
           .success('Error updating your job!');
