bf5e2bc5e04d2511988879653a49d8a2aa028bd6
sjacobs146 <susanjacobs@verizon.net>
2017-11-04 11:49:59 -0400

    Implement text-input component on form
    Refactor to use text-input for text input fields on form

--- app/components/job-input.js
+++ app/components/job-input.js
@@ -2,5 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['form-group']
+  classNames: ['form-group'],
+
+  statusList: [
+    {value: 'new', text: 'New'},
+    {value: 'applied', text: 'Applied'},
+    {value: 'inprogress', text: 'In Progress'},
+    {value: 'noresponse', text: 'No Response'},
+    {value: 'offer', text: 'Offer Made'},
+    {value: 'rejected', text: 'Rejected'},
+  ]
 });
