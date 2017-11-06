36f8f7dfe821ea2319e3faa697a012f698577c23
sjacobs146 <susanjacobs@verizon.net>
2017-11-06 13:56:36 -0500

    Clear sign-in and sign-up forms
    - Call init method to set credentials to {} in order to clear
    forms

--- app/components/sign-up-form.js
+++ app/components/sign-up-form.js
@@ -4,7 +4,10 @@ export default Ember.Component.extend({
   tagName: 'form',
   classNames: ['form-horizontal'],
 
-  credentials: {},
+  init() {
+      this._super(...arguments);
+      this.set('credentials', {});
+    },
 
   actions: {
     submit () {
--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -3,13 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
   flashMessages: Ember.inject.service(),
-
+  credentials: {},
   actions: {
     signUp (credentials) {
       this.get('auth').signUp(credentials)
       .then(() => this.get('auth').signIn(credentials))
       .then(() => this.transitionTo('application'))
       .then(() => {
+        this.set('credentials', {})
         this.get('flashMessages')
         .success('Successfully signed-up! You have also been signed-in.');
       })
